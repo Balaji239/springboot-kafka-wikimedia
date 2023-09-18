0e80441413d727ffa8f34dcf06fe05b80938846c
Balaji <baji2391999@gmail.com>
2023-09-18 12:46:27 +0530

    producer service setup

---
+++ kafka-wikimedia-producer/src/main/java/com/kakfawikimediaproducer/KafkaWikimediaProducerApplication.java
@@ -0,0 +1,11 @@
+package com.kakfawikimediaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWikimediaProducerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaWikimediaProducerApplication.class, args);
+    }
+}
