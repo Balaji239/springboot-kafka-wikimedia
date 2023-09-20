cf5f0070895a9057b1b442010ee7dc19b21dd495
Balaji <baji2391999@gmail.com>
2023-09-20 10:08:26 +0530

    implemented producer

--- kafka-wikimedia-producer/src/main/java/com/kakfawikimediaproducer/KafkaWikimediaProducerApplication.java
+++ kafka-wikimedia-producer/src/main/java/com/kakfawikimediaproducer/KafkaWikimediaProducerApplication.java
@@ -1,11 +1,22 @@
 package com.kakfawikimediaproducer;
 
+import com.kakfawikimediaproducer.kafka.WikimediaChangesProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaWikimediaProducerApplication {
+public class KafkaWikimediaProducerApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(KafkaWikimediaProducerApplication.class, args);
+    }
+
+    @Autowired
+    private WikimediaChangesProducer wikimediaChangesProducer;
+
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaChangesProducer.sendMessage();
     }
 }
